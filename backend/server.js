f97d4e77c6210bf8e44ed8a6f828d983c6dcdde0
Gevorg Alaverdyan <galaverdyan@genetec.com>
2022-12-10 21:40:13 -0500

    GET pendulums

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,21 @@ connectDB();
 
 const app = express();
 
+app.use(express.static("views"));
+app.use("/styles", express.static(__dirname + "views/styles"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("../views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  const time = new Date();
-  res.render('index', {time: time})
+//send data here
+app.get("/simulation", (req, res) => {
+  res.render("index");
 });
 
-app.use("/api/pendulums", require("./routes/userRoutes"));
+app.use("/api/pendulums", require("./routes/pendulumRoutes"));
 
 app.use(errorHandler);
 
@@ -27,3 +30,10 @@ app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
 
+////////////////////////
+
+//send requests from here
+
+app.get("/", (req, res) => {
+  res.render("config");
+});
